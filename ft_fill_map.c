85e4387010c293b20c1560e48a08f9e603962b58
djimmy LEVY <djimmydu91@gmail.com>
2015-02-11 12:10:54 +0100

    avant supr

--- ft_fill_map.c
+++ ft_fill_map.c
@@ -6,7 +6,7 @@
 /*   By: dlevy <dlevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/09 19:39:43 by dlevy             #+#    #+#             */
-/*   Updated: 2015/02/09 19:44:04 by dlevy            ###   ########.fr       */
+/*   Updated: 2015/02/10 14:59:30 by dlevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,5 @@ void	ft_fill_mapp(int **tab_i, char **tab_c, char **argv)
 		w = count_line(argv);
 		if (!(a_i = (int **)malloc(sizeof(int *) * w)))
 			return (NULL);
-	}
-	
-}
-
-void	create_map(int	fd, char **argv)
-{
-	int		c;
-	int		**tab_i;
-	char	*line;
-	char	**tab_c;
-
-	while (get_next_line(fd, &line) > 0)
-	{
-		mod_line(line);
-		tab_c = ft_strsplit(line, ' ');
-		ft_fill_map(tab_i, tab_c, argv);
 	}
 }
