b9acf875fb24d00a0cdd22400fe718941148cd6e
djimmy LEVY <djimmydu91@gmail.com>
2015-02-11 12:39:26 +0100

    maj

--- create_map.c
+++ create_map.c
@@ -1,11 +1,62 @@
-0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
-0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
-0  0 10 10  0  0 10 10  0  0  0 10 10 10 10 10  0  0  0
-0  0 10 10  0  0 10 10  0  0  0  0  0  0  0 10 10  0  0
-0  0 10 10  0  0 10 10  0  0  0  0  0  0  0 10 10  0  0
-0  0 10 10 10 10 10 10  0  0  0  0 10 10 10 10  0  0  0
-0  0  0 10 10 10 10 10  0  0  0 10 10  0  0  0  0  0  0
-0  0  0  0  0  0 10 10  0  0  0 10 10  0  0  0  0  0  0
-0  0  0  0  0  0 10 10  0  0  0 10 10 10 10 10 10  0  0
-0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
-0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlevy <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/31 12:53:05 by dlevy             #+#    #+#             */
+/*   Updated: 2015/02/10 18:06:35 by dlevy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+t_3dpos		**fill_map(t_list *lst, t_3dpos pt)
+{
+	int			i;
+	t_3dpos		**tab;
+
+	i = 0;
+	tab = (t_3dpos**)malloc(sizeof(t_3dpos*) * pt.y);
+	while (i < pt.y)
+	{
+		tab[i] = (t_3dpos*)malloc(sizeof(t_3dpos) * pt.x);
+		i++;
+	}
+	while (lst)
+	{
+		pt = *((t_3dpos*)lst->content);
+		tab[pt.y][pt.x] = pt;
+		lst = lst->next;
+	}
+	return (tab);
+}
+
+void		create_map(t_map map, int fd)
+{
+	int		i;
+	char	**tab;
+	char	*tmp;
+	t_list	*lst;
+
+	lst = NULL;
+	map.max.y = 0;
+	while (get_next_line(fd, &tmp))
+	{
+		i = 0;
+		map.max.x = 0;
+		tab = ft_strsplit(tmp, ' ');
+		while (tab[i])
+		{
+			map.max.z = ft_atoi(tab[i]);
+			ft_lstadd(&lst, ft_lstnew(&map.max, sizeof(t_3dpos)));
+			free(tab[i]);
+			i++;
+			map.max.x++;
+		}
+		map.max.y++;
+	}
+	map.map = fill_map(lst, map.max);
+	close(fd);
+}
--- ft_put_3d_map.c
+++ ft_put_3d_map.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_3d_line.c                                       :+:      :+:    :+:   */
+/*   ft_put_3d_map.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlevy <dlevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/02/10 18:27:17 by dlevy             #+#    #+#             */
-/*   Updated: 2015/02/10 19:09:02 by dlevy            ###   ########.fr       */
+/*   Created: 2015/02/10 17:50:45 by dlevy             #+#    #+#             */
+/*   Updated: 2015/02/10 19:12:47 by dlevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	ft_3d_line(t_3dpos pos1, t_3dpos pos2)
+void	ft_put_3d_map(t_map *map)
 {
-	
+	int		x;
+	int		y;
+
+	y = 0;
+	while (y < (map->max.y - 1))
+	{
+		x = 0;
+		while (x < (map->max.x - 1))
+		{
+			ft_3d_line(map.map[y][x], map.map[y][x + 1]);
+			ft_3d_line(map.map[y][x], map.map[y + 1][x]);
+			x++;
+		}
+		y++;
+	}
 }
